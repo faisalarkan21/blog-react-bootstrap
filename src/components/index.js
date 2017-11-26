f30251387cc9f9de69fbffb32b4268ff9c8bbc5c
faisal arkan <faisalarkan21@gmail.com>
2017-11-26 23:10:17 +0700

    add some text

--- src/components/index.js
+++ src/components/index.js
@@ -5,9 +5,13 @@ class Dashboard extends Component {
   render() {
     return (
       <div className >
-        <Col xs={4} md={4} >
+        <Col xs={4} md={5} lg={4} >
           <Panel header="Read Me" bsStyle="primary">
-            Hi there, i'm just making webpack config and basic example component <code>bootstrap paper with react and redux stuff</code>, so i can clone this repo if need it &nbsp; 😎.
+            Hi there, i'm just making webpack config and basic example component
+            <code>bootstrap paper with react and redux stuff</code>,
+            so i can clone this repo if need it &nbsp; 😎.
+            <br />
+           Feel free for use it on your own..  &nbsp;  🎧
           </Panel>
           <Button bsStyle="primary">hello !</Button>
         </Col>
