f4133ae78ec9f5575b81fb59ccad1de12913c816
faisal arkan <faisalarkan21@gmail.com>
2017-12-11 00:18:36 +0700

    make component alert.js

--- src/components/lib/alert.js
+++ src/components/lib/alert.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
 
+const AlertSuccess = props =>
+  (
+    <Alert bsStyle="success" className="text-center ">
+      <b> Selamat! </b> Anda telah terdaftar. <br />
+        Anda Dialihkan ke halaman login.
+    </Alert>
+  );
 
-const AlertInstance = () => (
-  <Alert bsStyle="warning">
-    <strong>Holy guacamole!</strong> Best check yo self, you're not looking too good.
-  </Alert>
-);
+
+const AlertError = props =>
+  (
+    <Alert bsStyle="danger" className="text-center ">
+      <b>Ups, Terjadi kesalahan! <br /> </b>
+      {props.isError}
+    </Alert>
+  );
+
+class ShowAlert extends React.Component {
+  render() {
+    const { status } = this.props;
+
+    if (status === undefined) {
+      return null;
+    }
+
+    return (
+      <div>
+        {status === true ? <AlertSuccess /> : <AlertError isError={status} /> }
+      </div>
+    );
+  }
+}
 
 
-export { AlertInstance };
+export { ShowAlert };
 
