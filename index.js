9df6ace74798c303a7b7555e434517bf1c047eac
faisal arkan <faisalarkan21@gmail.com>
2017-12-18 23:12:07 +0700

    set persistGate because state will delay if not use it.

--- index.js
+++ index.js
@@ -3,26 +3,31 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import path from 'path';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
-import store from './src/store/configureStore';
+import configureStore from './src/store/configureStore';
 import Root from './src/containers/root';
-
 
 /* eslint-disable */
 import 'typeface-roboto'; 
 import './public/style/base.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
+const { persistor, store } = configureStore();
+
 /* eslint-enable */
 ReactDOM.render(
 
-
   <Provider store={store}>
     <BrowserRouter>
-      <Root />
+      <PersistGate
+        loading={<div />}
+        persistor={persistor}
+      >
+        <Root />
+      </PersistGate>
     </BrowserRouter>
   </Provider>
-
   , document.getElementById('root'),
 );
 
