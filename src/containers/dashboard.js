0f3a799836298ee9848231619405d5617c3c69ee
faisal arkan <faisalarkan21@gmail.com>
2018-01-07 23:24:12 +0700

    dashboard check auth

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -3,21 +3,23 @@ import PropTypes from 'prop-types';
 import { withRouter, Redirect } from 'react-router-dom';
 import { AppNav } from '../components/lib';
 import NavBarContainer from './navbar-container';
-import { loadLogOut } from '../actions';
+import { loadLogOut, loadCheckAuth } from '../actions';
 import { connect } from 'react-redux';
 
 import DashboardComponent from '../components/dashboard-component';
 
 /* eslint no-use-before-define: ["error", { "functions": false }] */
-@connect(mapStateToProps, { loadLogOut })
+@connect(mapStateToProps, { loadLogOut, loadCheckAuth })
 @withRouter
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.handleLogOut = this.handleLogOut.bind(this);
+    this.props.loadCheckAuth();
   }
 
   componentWillReceiveProps(nextProps) {
+    console.log(this.props);
     const { result } = nextProps;
 
     if (!result.isLoginAuthenticated) {
