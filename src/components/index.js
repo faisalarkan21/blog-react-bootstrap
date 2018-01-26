429954d51f7465273dc4d07b951948f7b77831f9
faisal arkan <faisalarkan21@gmail.com>
2018-01-27 05:49:24 +0700

    fix index response testApi

--- src/components/index.js
+++ src/components/index.js
@@ -13,28 +13,24 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 class Dashboard extends Component {
   render() {
+    console.log(this.props);
     const {
-      linkLogin, linkDaftar, testApi, dataResponse,
+      testApi, dataResponse,
     } = this.props;
     return (
       <div>
-
         <Col id="post-panel" xs={5} md={5} lg={5}>
-
           <Panel bsStyle="primary">
             <Panel.Heading>Test Panel</Panel.Heading>
             <Panel.Body>
-              {dataResponse.message ?
-                dataResponse.message : 'Hey you there, Call Me ! (┛◉Д◉)┛┻━┻'}
+              {dataResponse.message === '' ?
+                 'Hey you there, Call Me ! (┛◉Д◉)┛┻━┻' : dataResponse.message }
               <br /> {dataResponse.database}
               <br /> {dataResponse.codeResponse}
             </Panel.Body>
           </Panel>
-
           <Button onClick={testApi} bsStyle="primary">Test API Connection</Button>
-
         </Col>
-
       </div>
     );
   }
--- src/containers/app.js
+++ src/containers/app.js
@@ -3,26 +3,30 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import NavBarContainer from './navbar-container';
-import { loadTestApi, loadLogOut } from '../actions';
+import { loadFetchApi, loadLogOut } from '../actions';
 import MainComponent from '../components';
 import { AppNav, IsLogged } from '../components/lib';
 
 /* eslint no-use-before-define: ["error", { "functions": false }] */
-@connect(mapStateToProps, { loadTestApi, loadLogOut })
+@connect(mapStateToProps, { loadFetchApi, loadLogOut })
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleLogOut = this.handleLogOut.bind(this);
+    this.handleFetchApi = this.handleFetchApi.bind(this);
   }
-
 
   handleLogOut() {
     this.props.loadLogOut();
   }
 
+  handleFetchApi() {
+    this.props.loadFetchApi();
+  }
+
   render() {
     console.log(this.props);
-    const { result, loadTestApi } = this.props;
+    const { result } = this.props;
 
     return (
       <div>
@@ -30,7 +34,7 @@ class App extends React.Component {
         <MainComponent
           linkDaftar="/daftar"
           linkLogin="/login"
-          testApi={loadTestApi}
+          testApi={this.handleFetchApi}
           dataResponse={result}
         />
       </div>
@@ -41,7 +45,7 @@ class App extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    result: state.callApi,
+    result: state.testApi,
   };
 }
 
